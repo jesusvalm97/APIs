cc5911b6c2fe90c29b6f21927c05d905cec09911
Jesús Antero Varela Almanza <52518344+jesusvalm97@users.noreply.github.com>
2022-08-02 12:49:33 -0500

    First api test with entity framework functionality

---
+++ WebAPIEFSample/WebAPIEFSample/Models/Libro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIEFSample.Models
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int Paginas { get; set; }
+    }
+}
