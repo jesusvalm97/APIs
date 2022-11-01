8c3d487c877fee3269c7df82c3871efa4668075e
Jesús Antero Varela Almanza <52518344+jesusvalm97@users.noreply.github.com>
2022-11-01 13:37:13 -0600

    Impelmentar el obtener productos por id o todos por meido de sql server

--- TestApi01/TestApi01/Repository/ProductosSQLServer.cs
+++ TestApi01/TestApi01/Repository/ProductosSQLServer.cs
@@ -10,13 +10,22 @@ namespace TestApi01.Repository
 {
     public class ProductosSQLServer : IProductosEnMemoria
     {
+        //Vaeriable para guardar la conexión a la base de datos
         private string CadenaConexion;
+
+        #region Constructores
 
         public ProductosSQLServer(AccesoDatos accesoDatos)
         {
             CadenaConexion = accesoDatos.CadenaConexionSQL;
         }
 
+        #endregion
+
+        /// <summary>
+        /// Crear una conexión de SQL en base a la variable CadenaConexion
+        /// </summary>
+        /// <returns></returns>
         private SqlConnection Conexion()
         {
             return new SqlConnection(CadenaConexion);
@@ -59,12 +68,82 @@ namespace TestApi01.Repository
 
         public Producto GetProducto(string SKU)
         {
-            throw new NotImplementedException();
+            Producto producto = null;
+
+            SqlConnection sqlConnection = Conexion();
+            SqlCommand sqlCommand = null;
+
+            try
+            {
+                sqlConnection.Open();
+                sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.CommandText = "dbo.ObtenerProductos";
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                sqlCommand.Parameters.Add("@SKU", SqlDbType.VarChar, 50).Value = SKU;
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                if (sqlDataReader.Read())
+                {
+                    producto = new Producto();
+                    producto.Nombre = sqlDataReader["Nombre"].ToString();
+                    producto.Descripcion = sqlDataReader["Descripcion"].ToString();
+                    producto.Precio = double.Parse(sqlDataReader["Precio"].ToString());
+                    producto.SKU = sqlDataReader["SKU"].ToString();
+                }
+            }
+            catch (Exception exception)
+            {
+                throw new Exception("Se produjo un error al dar de alta: " + exception.ToString());
+            }
+            finally
+            {
+                sqlCommand.Dispose();
+                sqlConnection.Close();
+                sqlConnection.Dispose();
+            }
+
+            return producto;
         }
 
         public IEnumerable<Producto> GetProductos()
         {
-            throw new NotImplementedException();
+            List<Producto> productos = new List<Producto>();
+            Producto producto = null;
+
+            SqlConnection sqlConnection = Conexion();
+            SqlCommand sqlCommand = null;
+
+            try
+            {
+                sqlConnection.Open();
+                sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.CommandText = "dbo.ObtenerProductos";
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                while (sqlDataReader.Read())
+                {
+                    producto = new Producto();
+                    producto.Nombre = sqlDataReader["Nombre"].ToString();
+                    producto.Descripcion = sqlDataReader["Descripcion"].ToString();
+                    producto.Precio = double.Parse(sqlDataReader["Precio"].ToString());
+                    producto.SKU = sqlDataReader["SKU"].ToString();
+                    productos.Add(producto);
+                }
+            }
+            catch (Exception exception)
+            {
+                throw new Exception("Se produjo un error al dar de alta: " + exception.ToString());
+            }
+            finally
+            {
+                sqlCommand.Dispose();
+                sqlConnection.Close();
+                sqlConnection.Dispose();
+            }
+
+            return productos;
         }
 
         public void ModificarProducto(Producto producto)
